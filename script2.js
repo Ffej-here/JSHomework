6c5152b71e0b162b2f963627497cd84f8fb8d4b7
Bouncy Donuts <eduard.alexiu@gmail.com>
2023-09-30 10:00:30 +0300

    no message

---
+++ script2.js
@@ -0,0 +1,5 @@
+let name = prompt("Enter your name");
+let gender = prompt("Enter Gender M/F");
+let height = prompt("Enter your height");
+let weight = prompt("Enter your weight");
+let BMI = weight / (height * height);
